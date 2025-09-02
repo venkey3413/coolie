9846c7f52ceffded904e6594d1bf89fce656e475
venkey3413 <venkatesh3413@gmail.com>
2025-09-02 14:05:29 +0530

    Fix admin server to show only admin panel

--- admin-server.js
+++ admin-server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('.'));
 
 // Initialize SQLite database
 const db = new sqlite3.Database('resort.db');
